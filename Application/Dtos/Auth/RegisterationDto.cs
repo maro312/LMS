6e7bdeb88cff230a4e741299ad11590b2dbbd2f4
maro312 <marawan.ah.ab@gmail.com>
2026-08-19 20:10:03 +0300

    add auth dtos

---
+++ Application/Dtos/Auth/RegisterationDto.cs
@@ -0,0 +1,12 @@
+using LMS.Domain.Constants;
+
+namespace Application.Dtos.Auth;
+
+public class RegisterationDto 
+{
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
+
