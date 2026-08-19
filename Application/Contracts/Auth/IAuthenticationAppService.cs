ed31e0f27b812733f6110cd088aec8cdf404e878
maro312 <marawan.ah.ab@gmail.com>
2026-08-19 20:12:47 +0300

    add auth service

---
+++ Application/Contracts/Auth/IAuthenticationAppService.cs
@@ -0,0 +1,11 @@
+using Application.Dtos.Auth;
+using LMS.Core.Results;
+
+namespace Application.Contracts.Auth;
+
+public interface IAuthenticationAppService
+{
+    Task<Result<AuthenticationDto>> Register(RegisterationDto dto);
+    Task<Result<AuthenticationDto>> Login(LoginDto dto);
+}
+
