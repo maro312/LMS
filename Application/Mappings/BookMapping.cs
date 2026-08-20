7c4cbbff9ad9a55e11bc697efa591e187031146b
maro312 <marawan.ah.ab@gmail.com>
2026-08-20 16:13:23 +0300

    feat: add book mapping

---
+++ Application/Mappings/BookMapping.cs
@@ -0,0 +1,68 @@
+using Application.Dtos.Books;
+using LMS.Domain.Entities;
+using System;
+
+namespace Application.Mappings;
+
+public static class BookMapping
+{
+    public static BookDto ToDto(this Book book)
+    {
+        if (book == null) return null!;
+
+        return new BookDto
+        {
+            Id = book.Id,
+            Isbn = book.Isbn,
+            Title = book.Title,
+            Author = book.Author,
+            CategoryId = book.CategoryId,
+            IsAvailable = book.IsAvailable,
+            TotalCopies = book.TotalCopies,
+            AvailableCopies = book.AvailableCopies,
+            BookPhotoUrl = book.BookPhotoUrl,
+            PublisherName = book.PublisherName,
+            PublishDate = book.PublishDate,
+            CreatedDate = book.CreatedDate,
+            CreatedBy = book.CreatedBy,
+            UpdatedDate = book.UpdatedDate,
+            ModifiedBy = book.ModifiedBy
+        };
+    }
+
+    public static Book ToEntity(this CreateUpdateBookDto dto)
+    {
+        if (dto == null) return null!;
+
+        return new Book
+        {
+            Id = Guid.NewGuid(),
+            Isbn = dto.Isbn,
+            Title = dto.Title,
+            Author = dto.Author,
+            CategoryId = dto.CategoryId,
+            IsAvailable = dto.IsAvailable,
+            TotalCopies = dto.TotalCopies,
+            AvailableCopies = dto.AvailableCopies,
+            PublisherName = dto.PublisherName,
+            PublishDate = dto.PublishDate
+        };
+    }
+
+    public static Book UpdateEntity(this CreateUpdateBookDto dto, Book book)
+    {
+        if (dto == null || book == null) return book!;
+
+        book.Isbn = dto.Isbn;
+        book.Title = dto.Title;
+        book.Author = dto.Author;
+        book.CategoryId = dto.CategoryId;
+        book.IsAvailable = dto.IsAvailable;
+        book.TotalCopies = dto.TotalCopies;
+        book.AvailableCopies = dto.AvailableCopies;
+        book.PublisherName = dto.PublisherName;
+        book.PublishDate = dto.PublishDate;
+
+        return book;
+    }
+}
