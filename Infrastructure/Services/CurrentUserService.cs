6802bae437e7b8bebbb675ab5bc00505e41e5e3a
maro312 <marawan.ah.ab@gmail.com>
2026-08-20 03:01:51 +0300

    feat: add current user server to access http context

---
+++ Infrastructure/Services/CurrentUserService.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace LMS.Infrastructure.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        private ClaimsPrincipal? User => _httpContextAccessor.HttpContext?.User;
+
+        public string? UserId => User?.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        public string? Email => User?.FindFirstValue(ClaimTypes.Email);
+
+        public bool IsAuthenticated => User?.Identity?.IsAuthenticated ?? false;
+
+        public string? GetClaim(string claimType)
+        {
+            return User?.FindFirstValue(claimType);
+        }
+    }
+}
