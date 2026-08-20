1879f8b1c69fccb0d0c6f9095b629a497c7f7696
maro312 <marawan.ah.ab@gmail.com>
2026-08-20 16:13:11 +0300

    feat: add book dtos

---
+++ Application/Dtos/Books/CreateUpdateBookDto.cs
@@ -0,0 +1,57 @@
+﻿using LMS.Domain.Lookups;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Dtos.Books;
+
+public class CreateUpdateBookDto
+{
+    /// <summary>
+    /// Optional ISBN
+    /// </summary>
+    public string? Isbn { get; set; }
+
+    /// <summary>
+    /// Book title
+    /// </summary>
+    public string Title { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Primary author name
+    /// </summary>
+    public string Author { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Category lookup reference
+    /// </summary>
+    public Guid? CategoryId { get; set; }
+
+    /// <summary>
+    /// Availability flag
+    /// </summary>
+    public bool IsAvailable { get; set; }
+
+    /// <summary>
+    /// Total library copies
+    /// </summary>
+    public int TotalCopies { get; set; }
+
+    /// <summary>
+    /// Copies currently available
+    /// </summary>
+    public int AvailableCopies { get; set; }
+
+    /// <summary>
+    /// Book photo file uploaded by the user. This property is optional and can be null if no file is uploaded.
+    /// </summary>
+    public IFormFile? BookPhotoFile { get; set; }
+
+    /// <summary>
+    /// The name of the publisher. This property is optional and can be null if the publisher's name is not available.
+    /// </summary>
+    public string? PublisherName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The date when the book was published. This property is optional and can be null if the publish date is not available.
+    /// </summary>
+    public DateOnly? PublishDate { get; set; }
+}
