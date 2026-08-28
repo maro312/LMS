03c5847499e1ca57f2809ee4d8a04134a9d2058c
maro312 <marawan.ah.ab@gmail.com>
2026-08-28 13:43:22 +0300

    refactor: book service

--- Application/Contracts/Books/IBookAppService.cs
+++ Application/Contracts/Books/IBookAppService.cs
@@ -6,4 +6,7 @@ namespace Application.Contracts.Books;
 
 public interface IBookAppService : ICrudAppService<CreateUpdateBookDto, Result<BookDto>, Guid, Result<IEnumerable<BookDto>>>
 {
+    Task<Result<PagedResult<BookDto>>> GetAllPaginatedAsync(int pageNumber, int pageSize);
+    Task<Result<IEnumerable<BookDto>>> SearchAsync(string keyword);
+    Task<Result<IEnumerable<BookDto>>> FilterAsync(Guid? categoryId, bool? isAvailable);
 }
